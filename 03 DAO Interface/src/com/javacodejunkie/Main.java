c0604aa08298ec4f2b9915f8dab85d8fd04e7ce8
David Purdy (aka Java Code Junkie) <45924598+JavaCodeJunkie@users.noreply.github.com>
2022-02-05 14:59:29 -0400

    Add files via upload

---
+++ 03 DAO Interface/src/com/javacodejunkie/Main.java
@@ -0,0 +1,11 @@
+package com.javacodejunkie;
+
+public class Main {
+
+	public static void main(String[] args) {
+		T employee = new T(1, 123456, "Sam", "Spade", 3);
+		
+		System.out.println(employee);
+	}
+
+}
