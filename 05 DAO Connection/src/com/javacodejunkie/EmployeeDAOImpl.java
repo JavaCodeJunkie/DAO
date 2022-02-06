da906202fd8cfdf0a88f7771ef43a92d4d6812e6
David Purdy (aka Java Code Junkie) <45924598+JavaCodeJunkie@users.noreply.github.com>
2022-02-06 15:52:52 -0400

    Add files via upload

---
+++ 05 DAO Connection/src/com/javacodejunkie/EmployeeDAOImpl.java
@@ -0,0 +1,50 @@
+package com.javacodejunkie;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+	// CRUD - Retrieve
+	@Override
+	public Employee get(int id) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	// CRUD - Retrieve All
+	@Override
+	public List<Employee> getAll() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	// CRUD - Create or Update
+	@Override
+	public int save(Employee employee) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	// CRUD - Create
+	@Override
+	public int insert(Employee employee) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	// CRUD - Update
+	@Override
+	public int update(Employee employee) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	// CRUD - Delete
+	@Override
+	public int delete(Employee employee) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
